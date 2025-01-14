a4dbb309e98be787e0ce7185b2f77d9055083355
tspence <1933486+tspence@users.noreply.github.com>
2025-01-14 16:36:26 +0000

    # Patch notes for 125.1.169
    These patch notes summarize the changes from version 122.0.214.
    
    Changes to data models:
    * WorkSpaceUserInfoDto: Added new field `permissions`

--- src/IProjectManagerClient.cs
+++ src/IProjectManagerClient.cs
@@ -1,15 +1,15 @@
 /***
  * ProjectManager API for C#
  *
- * (c) 2023-2024 ProjectManager.com, Inc.
+ * (c) 2023-2025 ProjectManager.com, Inc.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
  * @author     ProjectManager.com <support@projectmanager.com>
  *             
- * @copyright  2023-2024 ProjectManager.com, Inc.
- * @version    122.0.214
+ * @copyright  2023-2025 ProjectManager.com, Inc.
+ * @version    125.1.169
  * @link       https://github.com/projectmgr/projectmanager-sdk-csharp
  */
 

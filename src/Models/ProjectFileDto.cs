b9a589f0729bc6076f4c3864d1ffe5385150563e
Ted Spence <ted@spence.net>
2025-01-14 13:12:55 -0800

    Regenerate with latest version
    Remove copyright years
    Upgrade reference with System.Text.Json

--- src/Models/ProjectFileDto.cs
+++ src/Models/ProjectFileDto.cs
@@ -1,13 +1,13 @@
 /***
  * ProjectManager API for C#
  *
- * (c) 2023-2025 ProjectManager.com, Inc.
+ * (c) ProjectManager.com, Inc.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
  * @author     ProjectManager.com <support@projectmanager.com>
- * @copyright  2023-2025 ProjectManager.com, Inc.
+ * @copyright  ProjectManager.com, Inc.
  * @link       https://github.com/projectmgr/projectmanager-sdk-csharp
  */
 

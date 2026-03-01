23ae97299fe59a67764477babf4a34932e01ad48
tspence <1933486+tspence@users.noreply.github.com>
2026-03-01 00:49:35 +0000

    ## Patch notes for 150.1.171
    These patch notes summarize the changes from version 149.0.187.
    
    * Minor documentation changes, bugfixes, and performance improvements.

---
+++ src/Models/TaskApprovalStatusUpdateDto.cs
@@ -0,0 +1,35 @@
+/***
+ * ProjectManager API for C#
+ *
+ * (c) ProjectManager.com, Inc.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author     ProjectManager.com <support@projectmanager.com>
+ * @copyright  ProjectManager.com, Inc.
+ * @link       https://github.com/projectmgr/projectmanager-sdk-csharp
+ */
+
+
+
+#pragma warning disable CS8618
+
+using System;
+
+namespace ProjectManager.SDK.Models
+{
+
+    /// <summary>
+    /// Represents a request to update the approval status of a Task.
+    /// </summary>
+    public class TaskApprovalStatusUpdateDto : ApiModel
+    {
+
+        /// <summary>
+        /// The approval status to apply to the Task.
+        /// Must be one of: Approved, Denied, or Pending.
+        /// </summary>
+        public string Status { get; set; }
+    }
+}
